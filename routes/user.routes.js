69dfe8460c5cf8ce62fa5d91480a566233363538
Divyansh Arora <141400127+Aroraboy@users.noreply.github.com>
2026-01-31 03:19:50 +0530

    feat: add user retrieval endpoints with authorization middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { getUsers, getUser } from '../controllers/user.controller.js';
+import authorize from '../middlewares/auth.middleware.js';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({title: 'GET all users'}));
-userRouter.get('/:id', (req, res) => res.send({title: 'GET user by id'}));
+userRouter.get('/', authorize, getUsers);
+userRouter.get('/:id', authorize, getUser);
 userRouter.post('/', (req, res) => res.send({title: 'POST create user'}));
 userRouter.put('/:id', (req, res) => res.send({title: 'PUT update user by id'}));
 userRouter.delete('/:id', (req, res) => res.send({title: 'DELETE user by id'}));
