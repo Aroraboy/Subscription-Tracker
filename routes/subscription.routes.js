7b8f666799e8b539108fd8e7661aaab0ec9c12ce
Divyansh Arora <141400127+Aroraboy@users.noreply.github.com>
2026-01-31 11:02:44 +0530

    feat: add subscription creation and user subscription retrieval endpoints

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+import authorize from '../middlewares/auth.middleware.js';
+import { createSubscription } from '../controllers/subscription.controller.js';
+import { getUserSubscriptions } from '../controllers/subscription.controller';
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get('/', (req, res) => res.send({title: 'GET all subscriptions'}));
 subscriptionRouter.get('/:id', (req, res) => res.send({title: 'GET subscription by id'}));
-subscriptionRouter.post('/', (req, res) => res.send({title: 'POST create subscription'}));
+subscriptionRouter.post('/', authorize, createSubscription);
 subscriptionRouter.put('/:id', (req, res) => res.send({title: 'PUT update subscription by id'}));
 subscriptionRouter.delete('/:id', (req, res) => res.send({title: 'DELETE subscription by id'}));
-subscriptionRouter.get('/user/:id', (req, res) => res.send({title: 'GET all user subscriptions'}));
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 subscriptionRouter.put('/:id/cancel', (req, res) => res.send({title: 'PUT cancel subscription by id'}));
 subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send({title: 'GET all upcoming renewals'}));
 
